6a770a858d76803c287be02781866e7cb98d88a9
tjtyson1 <tjupnext@icloud.com>
2025-09-09 16:13:28 -0400

    Add files via upload

--- Correlation-Test.R
+++ Correlation-Test.R
@@ -4,3 +4,13 @@ if(!require(devtools)) install.packages("devtools") ;devtools::install_github("k
 
 # Load the library ggpubr
 library(ggpubr)
+
+#load some dummy dataset
+my_data <- mtcars
+
+#Print first few lines
+head(my_data)
+
+#Hypothesis: Weight of the car is inversely proportional to the mpg of the car
+
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
