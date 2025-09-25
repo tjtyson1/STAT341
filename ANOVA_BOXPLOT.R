334371922c6dd8e55e6ca310668abfc2ba5fb63f
tjtyson1 <tjupnext@icloud.com>
2025-09-25 16:20:12 -0400

    Add files via upload

--- ANOVA_BOXPLOT.R
+++ ANOVA_BOXPLOT.R
@@ -11,3 +11,17 @@ PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poiso
 df <- read.csv(PATH) %>% select(-X) %>% mutate(poison = factor(poison, ordered = TRUE)) 
 
 glimpse(df) 
+
+library(ggplot2)
+#Plot the dataset
+ggplot(df,aes(x=poison,y=time,fill=poison)) + geom_boxplot() + geom_jitter(shape=15,color="steelblue", position= position_jitter(0.21))+ theme_classic()
+
+anova_one_way <- aov(time~poison, data=df)
+summary(anova_one_way)
+
+
+     Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       2  1.033  0.5165   11.79 7.66e-05 ***
+Residuals   45  1.972  0.0438                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
